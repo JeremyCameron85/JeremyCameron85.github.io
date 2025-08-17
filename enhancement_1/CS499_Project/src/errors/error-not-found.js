ca1b1c088621844884bfa9e5557a1bac54104aa8
JeremyCameron <131938234+JeremyCameron85@users.noreply.github.com>
2025-08-17 16:15:30 -0400

    New branch

--- enhancement_1/CS499_Project/src/errors/error-not-found.js
+++
@@ -1,9 +0,0 @@
-// src/errors/NotFoundError.js
-// Error message when ID is not found
-export default class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'NotFoundError';
-    this.status = 404;
-  }
-}
