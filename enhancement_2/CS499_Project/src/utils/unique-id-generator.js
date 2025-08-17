6af84d1b4f6ae700f58decc32e3dd8fb5fed16ee
JeremyCameron <131938234+JeremyCameron85@users.noreply.github.com>
2025-08-17 16:16:02 -0400

    New branch

--- enhancement_2/CS499_Project/src/utils/unique-id-generator.js
+++
@@ -1,7 +0,0 @@
-// src/utils/id-generator.js
-import { randomUUID } from 'crypto';
-
-// Helper function for generating unique 10 character IDs. Creates a UUID, removes dashes, and then truncates it to 10 characters
-export function generateUniqueId() {
-  return randomUUID().replace(/-/g, '').substring(0, 10);
-}
