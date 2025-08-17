42279a601c11ab323b00543e3f99ceef885d0415
JeremyCameron <131938234+JeremyCameron85@users.noreply.github.com>
2025-08-17 16:14:32 -0400

    New branch

--- enhancement_2/CS499_Project/src/errors/error-validation.js
+++
@@ -1,9 +0,0 @@
-// src/errors/ValidationError.js
-// Error message when validation fails (For example, if input is blank or too long)
-export default class ValidationError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'ValidationError';
-    this.status = 400;
-  }
-}
--- enhancement_3/CS499_Project/src/errors/error-duplicate-id.js
+++
@@ -1,9 +0,0 @@
-// src/errors/DuplicateIdError.js
-// Error message when a duplicate ID is detected
-export default class DuplicateIdError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'DuplicateIdError';
-    this.status = 409;
-  }
-}
